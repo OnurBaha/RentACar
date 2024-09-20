a24575bf90b5f7af6832dcf503cf1b4a3b5ab7e1
OnurBaha <koconurbaha@gmail.com>
2024-09-21 02:26:52 +0300

    Added InMemoryCache implementation

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -16,6 +16,9 @@ public class Program
         builder.Services.AddControllers();
         builder.Services.AddApplicationServices();
         builder.Services.AddPersistenceServices(builder.Configuration);
+
+        builder.Services.AddDistributedMemoryCache();
+
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
